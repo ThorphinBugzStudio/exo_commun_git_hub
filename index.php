f6fbbab5c78a36f6476a6015cb25780fb8a10d8b
steven <steven.coic@gmail.com>
2017-06-19 16:51:07 +0200

    'toucheatoncul'

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
       <img src="images/SI_WiiUVC_SuperMetroid.jpg" alt="metroid">
       <img src="https://s-media-cache-ak0.pinimg.com/originals/60/d6/3b/60d63bdf611be9b67db790ab9c5c2576.jpg" alt="">
       <img src="http://www.verbicidemagazine.com/wp-content/uploads/2014/01/james-avery-fresh-prince.jpg" alt="">
-
+      <img src="vihxel.gif" alt="touche a ton cul">
     </div>
 
   </body>
