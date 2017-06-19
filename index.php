d08b09427a85a2eeba67dc693c6fd39ddf811ccc
Maximus <maximefauvelliere@gmail.com>
2017-06-19 15:55:15 +0200

    CSS MAx

--- index.php
+++ index.php
@@ -13,13 +13,13 @@
     <h1>Je suis bleu comme un oiseau bleu</h1>
 
     <h2>J' ai une git bite</h2>
-<<<<<<< HEAD
+
     <h2>pourquoi avez vous choisi cette branche ? parceque c est la plus belle et la plus solide #strategiedel'echec</h2>
     <p>Parce qu'on peut dire que vous êtes branchée, ma chère !</p>
-=======
+
 
     <h3>Ca me les casse à Caracasse</h3>
->>>>>>> da883bcad67f7930828f96b0bac1525274fed591
+
   </body>
 </html>
 <?php
