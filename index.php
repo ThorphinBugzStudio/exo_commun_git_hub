182f6820c2a5777e28f149baa9499ea9a43ee1dc
Vox <DarkBlueMax@hotmail.fr>
2017-06-19 17:01:09 +0200

    'lol'

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
-
-
-<?php echo 'Salut les coupain\'g' ?>
-
 <!DOCTYPE html>
 <html>
   <head>
@@ -10,6 +6,16 @@
     <title>exo_git_hub</title>
   </head>
   <body>
+    <nav>
+      <h1 class="mainTitle">Je suis un titre</h1>
+      <ul>
+        <li><a href="index.php">Accueil</a></li>
+        <li><a href="#">Lien</a></li>
+        <li><a href="#">Lien</a></li>
+        <li><a href="#">Lien</a></li>
+        <li><a href="#">Lien</a></li>
+      </ul>
+    </nav>
     <h1>Je suis bleu comme un oiseau bleu</h1>
 
     <h2>J' ai une git bite</h2>
@@ -22,11 +28,8 @@
 
     <div class="gallery">
       <img src="http://is3.mzstatic.com/image/thumb/Purple117/v4/d5/b3/9a/d5b39af6-6745-711c-37ea-3b705f4694f0/source/1200x630bb.jpg" alt="img">
-
       <img src="images/maxresdefault.jpg" alt="gitgud">
-
       <img src="https://s-media-cache-ak0.pinimg.com/originals/60/d6/3b/60d63bdf611be9b67db790ab9c5c2576.jpg">
-
       <img src="images/SI_WiiUVC_SuperMetroid.jpg" alt="metroid">
     </div>
     <center><img src="https://s-media-cache-ak0.pinimg.com/originals/60/d6/3b/60d63bdf611be9b67db790ab9c5c2576.jpg" alt=""></center>
