b0daf937868b76dee4dafdab49b8e570a80daad1
Maximus <maximefauvelliere@gmail.com>
2017-06-19 14:57:31 +0200

    modif index.php

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-<?php echo 'Salut les coupain\'g' ?> 
+
+<?php echo 'Salut les coupain\'g' ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>exo_git_hub</title>
+  </head>
+  <body>
+    <h1>Je suis bleu comme un oiseau bleu</h1>
+  </body>
+</html>
