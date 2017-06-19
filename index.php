e31f15561910047805432bb4a177f9feaa67c7e9
Maximus <maximefauvelliere@gmail.com>
2017-06-19 16:29:28 +0200

    Jazz est ds la place

--- index.php
+++ index.php
@@ -13,6 +13,14 @@
     <h1>Je suis bleu comme un oiseau bleu</h1>
 
     <h2>J' ai une git bite</h2>
+
+    <h2>pourquoi avez vous choisi cette branche ? parceque c est la plus belle et la plus solide #strategiedel'echec</h2>
+    <p>Parce qu'on peut dire que vous êtes branchée, ma chère !</p>
+    <h1>on dira ce qu on veut mais putain le ciel il est bleu</h1>
+
+    <h3>Ca me les casse à Caracasse</h3>
+    <img src="http://is3.mzstatic.com/image/thumb/Purple117/v4/d5/b3/9a/d5b39af6-6745-711c-37ea-3b705f4694f0/source/1200x630bb.jpg" alt="img">
+    <img src="/Users/Maximilia/Downloads/60d63bdf611be9b67db790ab9c5c2576.jpg" alt="">
   </body>
 </html>
 <?php
